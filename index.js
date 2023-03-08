7e967ff3cdaf1a289c57c96f1df21a58d93fcc34
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-08 22:44:40 +0100

    add searching animation and search button hover effect

--- index.js
+++ index.js
@@ -3,33 +3,41 @@ import Movie from './movie.js'
 const mainContainerEl = document.getElementById('main-container')
 const initialContainerEl = document.getElementById('initial-container')
 const resultsContainerEl = document.getElementById('results-container')
+const loadingEl = document.getElementById('lds-hourglass')
 
 
 document.getElementById('search-bar-btn').addEventListener('click', ()=> {
     resultsContainerEl.innerHTML = ''
     initialContainerEl.style.display = ''
+    initialContainerEl.innerHTML = 'Searching...'
     mainContainerEl.style.justifyContent = 'center'
+
     let searchTerm = document.getElementById('search-bar-input').value
-    
-    fetch(`http://www.omdbapi.com/?apikey=88ef49ef&s=${searchTerm}`)
-        .then(res => res.json())
-        .then(data => {
-            if(data.Response == 'False'){
-                initialContainerEl.innerHTML = "Unable to find what you’re looking for. Please try another search."
-            } else {
-                initialContainerEl.style.display = 'none'
-                mainContainerEl.style.justifyContent = 'flex-start'
-                
-                data.Search
-                    .map(result => new Movie(result))
-                    .forEach(item => {
-                        item.getFullMovieData()
-                            .then((movie) => {
-                                resultsContainerEl.innerHTML += movie.getHtml()
-                            })
-                    })
-            }
-        })
+
+    loadingEl.style.display = 'inline-block'
+    setTimeout(()=>{
+        loadingEl.style.display = 'none'
+        
+        fetch(`http://www.omdbapi.com/?apikey=88ef49ef&s=${searchTerm}`)
+            .then(res => res.json())
+            .then(data => {
+                if(data.Response == 'False'){
+                    initialContainerEl.innerHTML = "Unable to find what you’re looking for. Please try another search."
+                } else {
+                    initialContainerEl.style.display = 'none'
+                    mainContainerEl.style.justifyContent = 'flex-start'
+                    
+                    data.Search
+                        .map(result => new Movie(result))
+                        .forEach(item => {
+                            item.getFullMovieData()
+                                .then((movie) => {
+                                    resultsContainerEl.innerHTML += movie.getHtml()
+                                })
+                        })
+                }
+            })
+    }, 2000)
 })
 
 
