0b0bb2b68e44a9b048b4e391e4ef57caa9345216
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-08 22:31:15 +0100

    create variables for dom elements for cleaner code

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 import Movie from './movie.js'
 
+const mainContainerEl = document.getElementById('main-container')
+const initialContainerEl = document.getElementById('initial-container')
+const resultsContainerEl = document.getElementById('results-container')
+
+
 document.getElementById('search-bar-btn').addEventListener('click', ()=> {
-    document.getElementById('results-container').innerHTML = ''
-    document.getElementById('initial-container').style.display = ''
-    document.getElementById('main-container').style.justifyContent = 'center'
+    resultsContainerEl.innerHTML = ''
+    initialContainerEl.style.display = ''
+    mainContainerEl.style.justifyContent = 'center'
     let searchTerm = document.getElementById('search-bar-input').value
     
     fetch(`http://www.omdbapi.com/?apikey=88ef49ef&s=${searchTerm}`)
         .then(res => res.json())
         .then(data => {
             if(data.Response == 'False'){
-                document.getElementById('initial-container').innerHTML = "Unable to find what you’re looking for. Please try another search."
+                initialContainerEl.innerHTML = "Unable to find what you’re looking for. Please try another search."
             } else {
-                document.getElementById('initial-container').style.display = 'none'
-                document.getElementById('main-container').style.justifyContent = 'flex-start'
+                initialContainerEl.style.display = 'none'
+                mainContainerEl.style.justifyContent = 'flex-start'
                 
                 data.Search
                     .map(result => new Movie(result))
                     .forEach(item => {
                         item.getFullMovieData()
                             .then((movie) => {
-                                document.getElementById('results-container').innerHTML += movie.getHtml()
+                                resultsContainerEl.innerHTML += movie.getHtml()
                             })
                     })
             }
