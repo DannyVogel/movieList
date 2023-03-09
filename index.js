a58c822477971bed0f6704746c1e550813c2ebe5
DannyVogel <danny.hj.vogel@gmail.com>
2023-03-09 08:42:19 +0100

    include 2 second "searching..." animation with api fetch in background

--- index.js
+++ index.js
@@ -5,39 +5,43 @@ const initialContainerEl = document.getElementById('initial-container')
 const resultsContainerEl = document.getElementById('results-container')
 const loadingEl = document.getElementById('lds-hourglass')
 
-
 document.getElementById('search-bar-btn').addEventListener('click', ()=> {
-    resultsContainerEl.innerHTML = ''
-    initialContainerEl.style.display = ''
-    initialContainerEl.innerHTML = 'Searching...'
-    mainContainerEl.style.justifyContent = 'center'
-
     let searchTerm = document.getElementById('search-bar-input').value
 
+    resultsContainerEl.innerHTML = ''
+    resultsContainerEl.style.display = 'none'
+    initialContainerEl.style.display = 'none'
+    loadingEl.innerHTML = 'Searching...'
+    mainContainerEl.style.justifyContent = 'center'
     loadingEl.style.display = 'inline-block'
-    setTimeout(()=>{
-        loadingEl.style.display = 'none'
-        
-        fetch(`https://www.omdbapi.com/?apikey=88ef49ef&s=${searchTerm}`)
-            .then(res => res.json())
-            .then(data => {
-                if(data.Response == 'False'){
+    
+    fetch(`https://www.omdbapi.com/?apikey=88ef49ef&s=${searchTerm}`)
+        .then(res => res.json())
+        .then(data => {
+            if(data.Response == 'False'){
+                setTimeout(()=>{
+                    loadingEl.style.display = 'none'
+                    initialContainerEl.style.display = ''
                     initialContainerEl.innerHTML = "Unable to find what you’re looking for. Please try another search."
-                } else {
+                }, 2000)
+            } else {
+                setTimeout(()=>{
+                    loadingEl.style.display = 'none'
                     initialContainerEl.style.display = 'none'
                     mainContainerEl.style.justifyContent = 'flex-start'
-                    
-                    data.Search
-                        .map(result => new Movie(result))
-                        .forEach(item => {
-                            item.getFullMovieData()
-                                .then((movie) => {
-                                    resultsContainerEl.innerHTML += movie.getHtml()
-                                })
-                        })
-                }
-            })
-    }, 2000)
+                    resultsContainerEl.style.display = ''
+                }, 2000)
+                
+                data.Search
+                    .map(result => new Movie(result))
+                    .forEach(item => {
+                        item.getFullMovieData()
+                            .then((movie) => {
+                                resultsContainerEl.innerHTML += movie.getHtml()
+                            })
+                    })
+            }
+        })
 })
 
 
